e6f7f55bd8d3de3d94f5b3633eafc1dd09fd91de
Amodia, Karl Martin OOP 58002 <98940274+karlmartn@users.noreply.github.com>
2023-04-17 09:16:49 +0800

    Add files via upload

---
+++ Laboratory No.3.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+
+class MyWindow:
+    def __init__(self, win):
+
+        self.lbl1 = tk.Label(win, text="Laboratory Activity 5")
+        self.lbl1.place(x=200, y=20)
+        self.lbl2 = tk.Label(win, text="Submitted to:Mam Sayo")
+        self.lbl2.place(x=200, y=40)
+        self.lbl3 = tk.Label(win, text="<--- Click Here")
+        self.lbl3.place(x=250, y=120)
+
+        self.txt1 = tk.Entry(win, bd=1)
+        self.txt1.place(x=200, y=80)
+
+
+
+        def change_color():
+            fg_color = self.txt1.cget("foreground")
+            if button['bg'] == 'orange':
+                button['bg'] = 'blue'
+                if fg_color == 'black':
+                    self.txt1.config(fg='blue')
+                elif fg_color == 'blue':
+                    self.txt1.config(fg='orange')
+                else:
+                    self.txt1.config(fg='black')
+            else:
+                button['bg'] = 'orange'
+                if fg_color == 'black':
+                    self.txt1.config(fg='blue')
+                elif fg_color == 'blue':
+                    self.txt1.config(fg='orange')
+                else:
+                    self.txt1.config(fg='black')
+
+
+        button = tk.Button(win, text="Button", command=change_color)
+        button.place(x=200, y=120)
+
+
+root = tk.Tk()
+root.geometry("600x400+10+10")
+mywin = MyWindow(root)
+root.mainloop()
